jakubadler/sonatina

commit 9980ee4b67937356041d5106e48b9025076852fa

Library view sorting and handling untagged items.

src/library.c
@@ -2,6 +2,7 @@
 
 #include <glib.h>
 #include <gtk/gtk.h>
+#include <mpd/client.h>
 
 #include "library.h"
 #include "client.h"
@@ -66,7 +67,9 @@ gboolean library_tab_init(struct sonatina_tab *tab)
 	libtab->root = NULL;
 	libtab->path = NULL;
 
-	libtab->store = gtk_list_store_new(LIB_COL_COUNT, G_TYPE_STRING, G_TYPE_ICON, G_TYPE_STRING);
+	libtab->store = gtk_list_store_new(LIB_COL_COUNT, G_TYPE_STRING, G_TYPE_STRING, G_TYPE_ICON, G_TYPE_STRING);
+	gtk_tree_sortable_set_sort_column_id(GTK_TREE_SORTABLE(libtab->store), LIB_COL_DISPLAY_NAME, GTK_SORT_ASCENDING);
+
 	tw = gtk_builder_get_object(libtab->ui, "tw");
 	library_tw_set_columns(GTK_TREE_VIEW(tw));
 	gtk_tree_view_set_model(GTK_TREE_VIEW(tw), GTK_TREE_MODEL(libtab->store));
@@ -105,7 +108,7 @@ void library_tw_set_columns(GtkTreeView *tw)
 	gtk_tree_view_append_column(GTK_TREE_VIEW(tw), column);
 
 	renderer = gtk_cell_renderer_text_new();
-	column = gtk_tree_view_column_new_with_attributes("Name", renderer, "text", LIB_COL_NAME, NULL);
+	column = gtk_tree_view_column_new_with_attributes("Name", renderer, "text", LIB_COL_DISPLAY_NAME, NULL);
 	gtk_tree_view_append_column(GTK_TREE_VIEW(tw), column);
 }
 
@@ -138,6 +141,7 @@ void library_list_cb(GList *args, union mpd_cmd_answer *answer, void *data)
 	GList *cur;
 	enum listing_type type;
 	GtkTreeIter iter;
+	const gchar *name;
 
 	if (!args) {
 		MSG_WARNING("irrelevant list answer received");
@@ -157,9 +161,23 @@ void library_list_cb(GList *args, union mpd_cmd_answer *answer, void *data)
 
 	gtk_list_store_clear(tab->store);
 
-	for (cur = answer->list; cur; cur = cur->next) {
-		iter = library_model_append(tab->store, type, cur->data, NULL);
-		if (!g_strcmp0(tab->path->selected, cur->data)) {
+	for (cur = answer->list.list; cur; cur = cur->next) {
+		switch (type) {
+		case LIBRARY_GENRE:
+			name = ((struct mpd_tag_entity *) cur->data)->genre;
+			break;
+		case LIBRARY_ARTIST:
+			name = ((struct mpd_tag_entity *) cur->data)->artist;
+			break;
+		case LIBRARY_ALBUM:
+			name = ((struct mpd_tag_entity *) cur->data)->album;
+			break;
+		default:
+			name = NULL;
+			break;
+		}
+		iter = library_model_append(tab->store, type, name, NULL);
+		if (!g_strcmp0(tab->path->selected, name)) {
 			library_select(tab, &iter);
 		}
 	}
@@ -207,7 +225,7 @@ void library_lsinfo_cb(GList *args, union mpd_cmd_answer *answer, void *data)
 	}
 
 	gtk_list_store_clear(tab->store);
-
+	
 	for (cur = answer->lsinfo.list; cur; cur = cur->next) {
 		iter = library_model_append_entity(tab->store, cur->data);
 		if (!g_strcmp0(tab->path->selected, cur->data)) {
@@ -295,10 +313,31 @@ GtkTreeIter library_model_append(GtkListStore *model, enum listing_type type, co
 {
 	GtkTreeIter iter;
 	GIcon *icon;
+	const char *display;
+
+	if (strlen(name) == 0) {
+		switch (type) {
+		case LIBRARY_GENRE:
+			display = _("Unknown genre");
+			break;
+		case LIBRARY_ARTIST:
+			display = _("Unknown artist");
+			break;
+		case LIBRARY_ALBUM:
+			display = _("Unknown album");
+			break;
+		default:
+			display = _("Unknown");
+			break;
+		}
+	} else {
+		display = name;
+		name = NULL;
+	}
 
 	icon = g_icon_new_for_string(listing_icons[type], NULL);
 	gtk_list_store_append(model, &iter);
-	gtk_list_store_set(model, &iter, LIB_COL_ICON, icon, LIB_COL_NAME, name, LIB_COL_URI, uri, -1);
+	gtk_list_store_set(model, &iter, LIB_COL_ICON, icon, LIB_COL_DISPLAY_NAME, display, LIB_COL_NAME, name, LIB_COL_URI, uri, -1);
 
 	return iter;
 }
@@ -525,7 +564,7 @@ void library_tab_set_scroll(struct library_tab *tab)
 	gtk_tree_view_scroll_to_cell(GTK_TREE_VIEW(tw), tab->path->pos, NULL, TRUE, 0.0, 0.0);
 }
 
-void library_tab_open_dir(struct library_tab *tab, const char *name)
+void library_tab_open_dir(struct library_tab *tab, const char *display_name, const char *name)
 {
 	struct library_path *path;
 	const char *icon;
@@ -542,7 +581,7 @@ void library_tab_open_dir(struct library_tab *tab, const char *name)
 	} else {
 		icon = listing_icons[path->type];
 	}
-	sonatina_path_bar_open(tab->pathbar, name, icon);
+	sonatina_path_bar_open(tab->pathbar, display_name, icon);
 	library_load(tab);
 }
 
@@ -568,19 +607,26 @@ void library_clicked_cb(GtkTreeView *tw, GtkTreePath *path, GtkTreeViewColumn *c
 {
 	GtkTreeIter iter;
 	gchar *name;
+	gchar *display_name;
 
 	if (!gtk_tree_model_get_iter(GTK_TREE_MODEL(tab->store), &iter, path)) {
 		MSG_WARNING("library dir item vanished");
 		return;
 	}
 
-	gtk_tree_model_get(GTK_TREE_MODEL(tab->store), &iter, LIB_COL_NAME, &name, -1);
+	gtk_tree_model_get(GTK_TREE_MODEL(tab->store), &iter, LIB_COL_NAME, &name, LIB_COL_DISPLAY_NAME, &display_name, -1);
+
+	if (!name) {
+		name = display_name;
+	}
+
 	if (tab->path->selected) {
 		g_free(tab->path->selected);
 	}
+
 	tab->path->selected = name;
 
-	library_tab_open_dir(tab, name);
+	library_tab_open_dir(tab, display_name, name);
 }
 
 struct library_path *library_path_open(struct library_path *parent, const char *name)

src/client.c
@@ -170,7 +170,8 @@ struct mpd_cmd *mpd_cmd_new(enum mpd_cmd_type type)
 	case MPD_CMD_LIST:
 		cmd->parse_pair = parse_pair_list;
 		cmd->process = cmd_process_list;
-		cmd->answer.list = NULL;
+		cmd->answer.list.entity = NULL;
+		cmd->answer.list.list = NULL;
 		break;
 	default:
 		break;
@@ -181,8 +182,6 @@ struct mpd_cmd *mpd_cmd_new(enum mpd_cmd_type type)
 
 void mpd_cmd_free(struct mpd_cmd *cmd)
 {
-	GList *cur;
-
 	if (!cmd) {
 		return;
 	}
@@ -199,28 +198,19 @@ void mpd_cmd_free(struct mpd_cmd *cmd)
 		}
 		break;
 	case MPD_CMD_PLINFO:
-		for (cur = cmd->answer.plinfo.list; cur; cur = cur->next) {
-			mpd_song_free(cur->data);
-		}
-		g_list_free(cmd->answer.plinfo.list);
+		g_list_free_full(cmd->answer.plinfo.list, (GDestroyNotify) mpd_song_free);
 		break;
 	case MPD_CMD_LSINFO:
-		for (cur = cmd->answer.lsinfo.list; cur; cur = cur->next) {
-			mpd_entity_free(cur->data);
-		}
-		g_list_free(cmd->answer.lsinfo.list);
+		g_list_free_full(cmd->answer.lsinfo.list, (GDestroyNotify) mpd_entity_free);
 		break;
 	case MPD_CMD_LIST:
-		g_list_free_full(cmd->answer.list, g_free);
+		g_list_free_full(cmd->answer.list.list, (GDestroyNotify) mpd_tag_entity_free);
 		break;
 	default:
 		break;
 	}
 
-	for (cur = cmd->args; cur; cur = cur->next) {
-		g_free(cur->data);
-	}
-	g_list_free(cmd->args);
+	g_list_free_full(cmd->args, g_free);
 
 	g_free(cmd);
 }
@@ -258,7 +248,10 @@ void cmd_process_lsinfo(union mpd_cmd_answer *answer)
 
 void cmd_process_list(union mpd_cmd_answer *answer)
 {
-	answer->list = g_list_reverse(answer->list);
+	if (answer->list.entity) {
+		answer->list.list = g_list_prepend(answer->list.list, answer->list.entity);
+	}
+	answer->list.list = g_list_reverse(answer->list.list);
 }
 
 gboolean parse_pair_status(union mpd_cmd_answer *answer, const struct mpd_pair *pair)
@@ -333,7 +326,12 @@ gboolean parse_pair_lsinfo(union mpd_cmd_answer *answer, const struct mpd_pair *
 
 gboolean parse_pair_list(union mpd_cmd_answer *answer, const struct mpd_pair *pair)
 {
-	answer->list = g_list_prepend(answer->list, g_strdup(pair->value));
+	if (!answer->list.entity) {
+		answer->list.entity = mpd_tag_entity_begin(pair);
+	} else if (!mpd_tag_entity_feed(answer->list.entity, pair)) {
+		answer->list.list = g_list_prepend(answer->list.list, answer->list.entity);
+		answer->list.entity = mpd_tag_entity_begin(pair);
+	}
 
 	return TRUE;
 }

src/client.h
@@ -4,6 +4,8 @@
 #include <glib.h>
 #include <mpd/client.h>
 
+#include "util.h"
+
 enum mpd_cmd_type {
 	MPD_CMD_NONE,
 	MPD_CMD_CURRENTSONG,
@@ -44,7 +46,10 @@ union mpd_cmd_answer {
 		struct mpd_entity *entity;
 		GList *list;
 	} lsinfo; /* MPD_CMD_LSINFO */
-	GList *list; /* MPD_CMD_LIST */
+	struct {
+		struct mpd_tag_entity *entity;
+		GList *list;
+	} list; /* MPD_CMD_LIST */
 	int idle; /* MPD_CMD_IDLE */
 	gboolean ok;
 };

src/library.h
@@ -51,9 +51,10 @@ struct library_tab {
   Columns of the list store
   */
 enum lib_columns {
-	LIB_COL_NAME,
-	LIB_COL_ICON,
-	LIB_COL_URI,
+	LIB_COL_DISPLAY_NAME, /* name to be displayed */
+	LIB_COL_NAME, /* actual name if it differs from display name (e.g. empty tags displayed as 'Unknown') */
+	LIB_COL_ICON, /* icon to be displayed */
+	LIB_COL_URI, /* URI if this is a mpd entity */
 	LIB_COL_COUNT
 };
 
@@ -87,9 +88,10 @@ void library_tw_set_columns(GtkTreeView *tw);
   @brief Open directory identified by name in the currently opened library tree
   node.
   @param tab Library tab.
-  @param name Name of the item to be opened.
+  @param display_name Name to be displayed on the pathbar.
+  @param name Actual ame of the item to be opened.
   */
-void library_tab_open_dir(struct library_tab *tab, const char *name);
+void library_tab_open_dir(struct library_tab *tab, const char *display_name, const char *name);
 
 /**
   @brief Save scrollbar position in current view.

src/util.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
+
+#include <glib.h>
 
 #include "util.h"
+#include "gettext.h"
 
 #ifdef DEBUG
 int log_level = LEVEL_DEBUG;
@@ -69,3 +73,71 @@ GtkBuilder *load_tab_ui(const char *name)
 
 	return ui;
 }
+
+struct mpd_tag_entity *mpd_tag_entity_begin(const struct mpd_pair *pair)
+{
+	struct mpd_tag_entity *entity;
+
+	entity = g_malloc(sizeof(struct mpd_tag_entity));
+
+	entity->genre = NULL;
+	entity->artist = NULL;
+	entity->album = NULL;
+	entity->date = NULL;
+
+	mpd_tag_entity_feed(entity, pair);
+
+	return entity;
+}
+
+gboolean mpd_tag_entity_feed(struct mpd_tag_entity *entity, const struct mpd_pair *pair)
+{
+	g_assert(entity != NULL);
+	g_assert(pair != NULL);
+
+	if (!g_strcmp0(pair->name, "Genre")) {
+		if (!entity->genre) {
+			entity->genre = g_strdup(pair->value);
+			return TRUE;
+		}
+	} else if (!g_strcmp0(pair->name, "Artist")) {
+		if (!entity->artist) {
+			entity->artist = g_strdup(pair->value);
+			return TRUE;
+		}
+	} else if (!g_strcmp0(pair->name, "Album")) {
+		if (!entity->album) {
+			entity->album = g_strdup(pair->value);
+			return TRUE;
+		}
+	} else if (!g_strcmp0(pair->name, "Date")) {
+		if (!entity->date) {
+			entity->date = g_strdup(pair->value);
+			return TRUE;
+		}
+	}
+
+	return FALSE;
+}
+
+void mpd_tag_entity_free(struct mpd_tag_entity *entity)
+{
+	if (entity->genre) {
+		g_free(entity->genre);
+	}
+
+	if (entity->artist) {
+		g_free(entity->artist);
+	}
+
+	if (entity->album) {
+		g_free(entity->album);
+	}
+
+	if (entity->date) {
+		g_free(entity->date);
+	}
+
+	g_free(entity);
+}
+

src/util.h
@@ -2,6 +2,7 @@
 #define UTIL_H
 
 #include <gtk/gtk.h>
+#include <mpd/client.h>
 
 enum {
 	LEVEL_DEBUG,
@@ -26,6 +27,46 @@ int printmsg(int level, const char *format, ...);
 #define MSG_ERROR(...) { printmsg(LEVEL_ERROR, __VA_ARGS__); }
 #define MSG_FATAL(...) { printmsg(LEVEL_FATAL, __VA_ARGS__); }
 
+
+/**
+  An entity defined by MPD tags. Depending on which fields are used, this entity
+  represents either a genre, an artist or an album. Functions that work with
+  this structure resemble libmpd's API for entities.
+  */
+struct mpd_tag_entity {
+	gchar *genre;
+	gchar *artist;
+	gchar *album;
+	gchar *date;
+};
+
+struct mpd_tag_entity *mpd_tag_entity_begin(const struct mpd_pair *pair);
+gboolean mpd_tag_entity_feed(struct mpd_tag_entity *entity, const struct mpd_pair *pair);
+void mpd_tag_entity_free(struct mpd_tag_entity *entity);
+
 GtkBuilder *load_tab_ui(const char *name);
+
+/**
+  @brief Begin parsing a new entity.
+  @param pair First pair in this entity.
+  @returns Newly allocated mpd_tag_entity.
+  */
+struct mpd_tag_entity *mpd_tag_entity_begin(const struct mpd_pair *pair);
+
+/**
+  @brief Parse pair and add its information to the entity.
+  @param entity @a mpd_tag_entity returned by @a mpd_tag_entity_begin().
+  @param pair New pair to parse.
+  @returns TRUE if the pair was parsed and added to the entity (or if the pair
+  was not understood and ignored), FALSE if this pair is the beginning of a next
+  entity.
+  */
+gboolean mpd_tag_entity_feed(struct mpd_tag_entity *entity, const struct mpd_pair *pair);
+
+/**
+  @brief Free @a mpd_tag_entity allocated by @a mpd_tag_entity_begin().
+  @param entity A @a mpd_tag_entity object.
+  */
+void mpd_tag_entity_free(struct mpd_tag_entity *entity);
 
 #endif

commit 8183e1087008920eabfbdf590008e58037477c3c

Fix issue caused by 9980ee4b67937356041d5106e48b9025076852fa

src/library.c
@@ -726,6 +726,7 @@ gboolean library_add_selected(struct library_tab *tab, GtkTreeSelection *selecti
 	GList *rows;
 	GList *cur;
 	gchar *name;
+	gchar *display_name;
 	gchar *uri;
 	gboolean retval = TRUE;
 
@@ -735,14 +736,18 @@ gboolean library_add_selected(struct library_tab *tab, GtkTreeSelection *selecti
 		if (!gtk_tree_model_get_iter(model, &iter, cur->data)) {
 			continue;
 		}
-		gtk_tree_model_get(model, &iter, LIB_COL_NAME, &name, LIB_COL_URI, &uri, -1);
+		gtk_tree_model_get(model, &iter, LIB_COL_DISPLAY_NAME, &display_name, LIB_COL_NAME, &name, LIB_COL_URI, &uri, -1);
 		if (uri) {
 			retval = retval && library_add(tab, uri);
-			g_free(uri);
-		} else {
+		} else if (name) {
 			retval = retval && library_add(tab, name);
+		} else {
+			retval = retval && library_add(tab, display_name);
 		}
+
+		g_free(uri);
 		g_free(name);
+		g_free(display_name);
 	}
 
 	g_list_free_full(rows, (GDestroyNotify) gtk_tree_path_free);